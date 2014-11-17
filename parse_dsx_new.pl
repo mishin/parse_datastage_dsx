b0a150547cc33828c1f78368503ace4d8642e77a
Nikolay Mishin <mi@ya.ru>
2014-11-17 10:33:35 +0300

    Update parse_dsx_new.pl

--- parse_dsx_new.pl
+++ parse_dsx_new.pl
@@ -13,10 +13,17 @@ use Data::Printer {
     return_value   => 'pass',
 };
 
-enc_terminal();
 
-my ($file_name) = 'orchestrate_code_body.xml';
-my $data = read_file($file_name);
+main();
+
+sub main {
+    enc_terminal();
+    my $file_name  = 'orchestrate_code_body.xml';
+    my $data       = read_file($file_name);
+    my $parsed_dsx = start_parse($data);
+    display_dsx_content($parsed_dsx, $file_name);
+}
+
 
 sub make_regexp {
     my $operator_rx      = qr{\Q#### STAGE: \E(?<stage_name>\w+)};
@@ -36,114 +43,122 @@ sub make_regexp {
     return $ORCHESTRATE_BODY_RX;
 }
 
-my $parsed_dsx = start_parse($data);
-display_dsx_content($parsed_dsx);
+#
+# New subroutine "head_of_stage" extracted - Mon Nov 17 10:15:21 2014.
+#
+sub head_of_stage {
+    my ($t, $i, $stage) = @_;
 
-sub display_dsx_content {
-    my $parsed_dsx = shift;
+    my ($in, $in_type, $out, $out_type) = ('', '', '', '');
+    if ($stage->{ins}->{in} eq 'yes') {
+        $in      = join "\n", $_->{link_name} for @{$stage->{ins}->{inputs}};
+        $in_type = join "\n", $_->{link_type} for @{$stage->{ins}->{inputs}};
+
+    }
+
+    if ($stage->{ins}->{out} eq 'yes') {
+        $out = join "\n", $_->{link_name} for @{$stage->{ins}->{outputs}};
+        $out_type = join "\n", $_->{link_type}
+          for @{$stage->{ins}->{outputs}};
+
+    }
+    $t->addRow($i, $stage->{stage_name}, $stage->{operator_name},
+        $in, '', '', '', '', $out, '', '', '', '');
+    return $t;
+}
+
+#
+# New subroutine "show_in_fields" extracted - Mon Nov 17 10:20:07 2014.
+#
+sub show_in_fields {
+    my $t     = shift;
+    my $stage = shift;
+
+    if ($stage->{ins}->{in} eq 'yes'
+        && ${$stage->{ins}->{inputs}}[0]->{is_param} eq 'yes')
+    {
+        $t->addRowLine();
+        my $j = 1;
+        for my $f (@{${$stage->{ins}->{inputs}}[0]->{params}}) {
+            $t->addRow('', '', '', '', $j, $f->{field_name}, $f->{field_type},
+                $f->{is_null}, '', '', '', '', '');
+            $t->addRowLine();
+            $j++;
+        }
+    }
+
+    return $t;
+}
+
+
+#
+# New subroutine "show_out_fields" extracted - Mon Nov 17 10:20:53 2014.
+#
+sub show_out_fields {
+    my $t     = shift;
+    my $stage = shift;
+
+    if ($stage->{ins}->{out} eq 'yes'
+        && ${$stage->{ins}->{outputs}}[0]->{is_param} eq 'yes')
+    {
+        $t->addRowLine();
+        my $y = 1;
+        for my $f (@{${$stage->{ins}->{outputs}}[0]->{params}}) {
+            $t->addRow('', '', '', '', '', '', '', '', '', $y,
+                $f->{field_name}, $f->{field_type}, $f->{is_null});
+            $t->addRowLine();
+            $y++;
+        }
+    }
+    return $t;
+}
+
+
+#
+# New subroutine "display_main_header" extracted - Mon Nov 17 10:30:17 2014.
+#
+sub display_main_header {
+    my $file_name = shift;
 
     my $t = Text::ASCIITable->new(
-        { headingText => 'Parsing ORCHESTRATE of ' . $file_name } );
+        {headingText => 'Parsing ORCHESTRATE of ' . $file_name});
     $t->setCols(
         'Id',      'stage_name', 'op_name',    'inputs',
         'num',     'field_name', 'field_type', 'is_null',
         'outputs', 'num',        'field_name', 'field_type',
         'is_null'
     );
+    return $t;
+}
+
+sub display_dsx_content {
+    my ($parsed_dsx, $file_name) = @_;
+
+    my $t = display_main_header($file_name);
+
     my $i = 1;
-    foreach my $stage ( @{$parsed_dsx} ) {
+    foreach my $stage (@{$parsed_dsx}) {
+        if ($stage->{stage_name} eq 'T199') {
+            $t = head_of_stage($t, $i, $stage);
+            $t = show_in_fields($t, $stage);
+            $t = show_out_fields($t, $stage);
 
-        #  if (
-        #      $stage->{operator_name} eq 'copy'
-
-        #&& $stage->{stage_name} eq 'DWH_REESTRS_DS'
-        #    )
-        # {
-        #p $stage;
-        my ( $in, $in_type, $out, $out_type ) = ( '', '', '', '' );
-        if ( $stage->{ins}->{in} eq 'yes' ) {
-            $in = join "\n", $_->{link_name} for @{ $stage->{ins}->{inputs} };
-            $in_type = join "\n", $_->{link_type}
-              for @{ $stage->{ins}->{inputs} };
-
-        }
-
-        if ( $stage->{ins}->{out} eq 'yes' ) {
-            $out = join "\n", $_->{link_name} for @{ $stage->{ins}->{outputs} };
-            $out_type = join "\n", $_->{link_type}
-              for @{ $stage->{ins}->{outputs} };
-
-        }
-        $t->addRow( $i, $stage->{stage_name}, $stage->{operator_name},
-            $in, '', '', '', '', $out, '', '', '', '' );
-        if ( ${ $stage->{ins}->{inputs} }[0]->{is_param} eq 'yes' ) {
             $t->addRowLine();
-            my $j = 1;
-            for my $f ( @{ ${ $stage->{ins}->{inputs} }[0]->{params} } ) {
-                $t->addRow( '', '', '', '', $j, $f->{field_name},
-                    $f->{field_type}, $f->{is_null}, '', '', '', '', '' );
-                $t->addRowLine();
-                $j++;
-            }
+            $i++;
         }
-
-        if ( ${ $stage->{ins}->{outputs} }[0]->{is_param} eq 'yes' ) {
-            $t->addRowLine();
-            my $y = 1;
-            for my $f ( @{ ${ $stage->{ins}->{outputs} }[0]->{params} } ) {
-                $t->addRow( '', '', '', '', '', '', '', '', '', $y,
-                    $f->{field_name}, $f->{field_type}, $f->{is_null} );
-                $t->addRowLine();
-                $y++;
-            }
-        }
-        $t->addRowLine();
-        $i++;
-
-        # }
-
     }
-
     print $t;
 }
 
-sub display_fields {
-    my $fields = shift;
-
-    #my @f=@$fields;
-    #p @f;
-    #say $f[0]->{is_param};
-    if ( $$fields[0]->{is_param} eq 'yes' ) {
-        my $fields_table = Text::ASCIITable->new(
-            { headingText => 'Fields of ' . $$fields[0]->{link_name} } );
-        $fields_table->setCols( 'field_name', 'field_type', 'is_null' );
-        my $i = 1;
-
-        #p $$fields[0]->{params};
-        my @fields_of_link = @{ $$fields[0]->{params} };
-        for my $f (@fields_of_link) {
-            p $f;
-            print ref $f;
-
-            #p $f->{field_name};
-            say
-"$fields_table->addRow( $i,$f->{field_name} ,$f->{field_type},$f->{is_null});	";
-
-            #$fields_table->addRowLine();
-            $i++;
-        }
-        print $fields_table;
-    }
-}
 
 sub start_parse {
     my $data                = shift;
     my $ORCHESTRATE_BODY_RX = make_regexp();
     local $/ = '';
     my @parsed_dsx = ();
-    while ( $data =~ m/$ORCHESTRATE_BODY_RX/xsg ) {
+    while ($data =~ m/$ORCHESTRATE_BODY_RX/xsg) {
         my %stage = ();
-        my $ins   = process_stage_body( $+{stage_body} );
+        my $ins   = process_stage_body($+{stage_body});
         $stage{ins}           = $ins;
         $stage{stage_name}    = $+{stage_name};
         $stage{operator_name} = $+{operator_name};
@@ -168,17 +183,17 @@ sub process_stage_body {
     my $inputs_rx  = qr{## Inputs\n(?<inputs_name>.*?)(?:#|^;$)}sm;
     my $outputs_rx = qr{## Outputs\n(?<outputs_name>.*?)^;$}sm;
 
-    my ( $inputs, $outputs ) = ( '', '' );
+    my ($inputs, $outputs) = ('', '');
     $outs{in}   = 'no';
     $outs{out}  = 'no';
     $outs{body} = $stage_body;
 
-    if ( $stage_body =~ $inputs_rx ) {
-        $outs{inputs} = get_inout_links( $+{inputs_name} );
+    if ($stage_body =~ $inputs_rx) {
+        $outs{inputs} = get_inout_links($+{inputs_name});
         $outs{in}     = 'yes';
     }
-    if ( $stage_body =~ $outputs_rx ) {
-        $outs{outputs} = get_inout_links( $+{outputs_name} );
+    if ($stage_body =~ $outputs_rx) {
+        $outs{outputs} = get_inout_links($+{outputs_name});
         $outs{out}     = 'yes';
     }
     return \%outs;
@@ -210,16 +225,16 @@ sub get_inout_links {
 		)' 
 		      }xs;
 
-    while ( $body =~ m/$link/g ) {
+    while ($body =~ m/$link/g) {
         my %link_param = ();
         $link_param{link_name} = $+{link_name};
         $link_param{link_type} = $+{link_type};
         $link_param{is_param}  = 'no';
-        if ( length( $link_param{link_type} ) >= 6
-            && substr( $link_param{link_type}, 0, 6 ) eq 'modify' )
+        if (length($link_param{link_type}) >= 6
+            && substr($link_param{link_type}, 0, 6) eq 'modify')
         {
             $link_param{is_param} = 'yes';
-            $link_param{params}   = get_fields( $+{link_type} );
+            $link_param{params}   = get_fields($+{link_type});
         }
         push @links, \%link_param;
     }
@@ -242,7 +257,7 @@ sub get_fields {
     ;
      }xs;
 
-    while ( $body_for_fields =~ m/$field/g ) {
+    while ($body_for_fields =~ m/$field/g) {
         my %field_param = ();
         $field_param{field_name} = $+{field_name};
         $field_param{is_null}    = $+{is_null};
@@ -256,9 +271,9 @@ sub get_fields {
 sub enc_terminal {
 
     if (-t) {
-        binmode( STDIN,  ":encoding(console_in)" );
-        binmode( STDOUT, ":encoding(console_out)" );
-        binmode( STDERR, ":encoding(console_out)" );
+        binmode(STDIN,  ":encoding(console_in)");
+        binmode(STDOUT, ":encoding(console_out)");
+        binmode(STDERR, ":encoding(console_out)");
     }
 }
 
