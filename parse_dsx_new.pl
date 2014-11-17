ac3d29abd33e76303307893c6824bbb8ed4f2cb1
Nikolay Mishin <mi@ya.ru>
2014-11-17 18:25:13 +0300

    Update parse_dsx_new.pl

--- parse_dsx_new.pl
+++ parse_dsx_new.pl
@@ -20,8 +20,8 @@ sub main {
     enc_terminal();
     my $file_name  = 'orchestrate_code_body.xml';
     my $data       = read_file($file_name);
-    my $parsed_dsx = start_parse($data);
-    display_dsx_content($parsed_dsx, $file_name);
+    my $parsed_dsx = parse_orchestrate_body($data);
+    show_dsx_content($parsed_dsx, $file_name);
 }
 
 
@@ -46,21 +46,19 @@ sub make_regexp {
 #
 # New subroutine "head_of_stage" extracted - Mon Nov 17 10:15:21 2014.
 #
-sub head_of_stage {
+sub show_head_of_stage {
     my ($t, $i, $stage) = @_;
 
     my ($in, $in_type, $out, $out_type) = ('', '', '', '');
     if ($stage->{ins}->{in} eq 'yes') {
-        $in      = join "\n", $_->{link_name} for @{$stage->{ins}->{inputs}};
-        $in_type = join "\n", $_->{link_type} for @{$stage->{ins}->{inputs}};
-
+        for (@{$stage->{ins}->{inputs}}) {
+            $in = $in . $_->{link_name} . "\n";
+        }
     }
-
     if ($stage->{ins}->{out} eq 'yes') {
-        $out = join "\n", $_->{link_name} for @{$stage->{ins}->{outputs}};
-        $out_type = join "\n", $_->{link_type}
-          for @{$stage->{ins}->{outputs}};
-
+        for (@{$stage->{ins}->{outputs}}) {
+            $out = $out . $_->{link_name} . "\n";
+        }
     }
     $t->addRow($i, $stage->{stage_name}, $stage->{operator_name},
         $in, '', '', '', '', $out, '', '', '', '');
@@ -117,7 +115,7 @@ sub show_out_fields {
 #
 # New subroutine "display_main_header" extracted - Mon Nov 17 10:30:17 2014.
 #
-sub display_main_header {
+sub show_main_header {
     my $file_name = shift;
 
     my $t = Text::ASCIITable->new(
@@ -131,34 +129,36 @@ sub display_main_header {
     return $t;
 }
 
-sub display_dsx_content {
+sub show_dsx_content {
     my ($parsed_dsx, $file_name) = @_;
 
-    my $t = display_main_header($file_name);
+    my $t = show_main_header($file_name);
 
     my $i = 1;
     foreach my $stage (@{$parsed_dsx}) {
-        if ($stage->{stage_name} eq 'T199') {
-            $t = head_of_stage($t, $i, $stage);
+        if ($stage->{stage_name} eq 'LJ108') {
+
+            # p $stage;
+            $t = show_head_of_stage($t, $i, $stage);
             $t = show_in_fields($t, $stage);
             $t = show_out_fields($t, $stage);
 
             $t->addRowLine();
             $i++;
-        }
+         }
     }
     print $t;
 }
 
 
-sub start_parse {
+sub parse_orchestrate_body {
     my $data                = shift;
     my $ORCHESTRATE_BODY_RX = make_regexp();
     local $/ = '';
     my @parsed_dsx = ();
     while ($data =~ m/$ORCHESTRATE_BODY_RX/xsg) {
         my %stage = ();
-        my $ins   = process_stage_body($+{stage_body});
+        my $ins   = parse_stage_body($+{stage_body});
         $stage{ins}           = $ins;
         $stage{stage_name}    = $+{stage_name};
         $stage{operator_name} = $+{operator_name};
@@ -167,19 +167,9 @@ sub start_parse {
     return \@parsed_dsx;
 }
 
-sub process_stage_body {
+sub parse_stage_body {
     my ($stage_body) = @_;
     my %outs;
-
-=pod
-## Inputs
-0< [] 'LJ108:L109.v'
-## Outputs
-0> [] 'T199:INS.v'
-1> [] 'T199:UPD.v'
-;
-=cut    
-
     my $inputs_rx  = qr{## Inputs\n(?<inputs_name>.*?)(?:#|^;$)}sm;
     my $outputs_rx = qr{## Outputs\n(?<outputs_name>.*?)^;$}sm;
 
@@ -187,19 +177,18 @@ sub process_stage_body {
     $outs{in}   = 'no';
     $outs{out}  = 'no';
     $outs{body} = $stage_body;
-
     if ($stage_body =~ $inputs_rx) {
-        $outs{inputs} = get_inout_links($+{inputs_name});
+        $outs{inputs} = parse_inout_links($+{inputs_name});
         $outs{in}     = 'yes';
     }
     if ($stage_body =~ $outputs_rx) {
-        $outs{outputs} = get_inout_links($+{outputs_name});
+        $outs{outputs} = parse_inout_links($+{outputs_name});
         $outs{out}     = 'yes';
     }
     return \%outs;
 }
 
-sub get_inout_links {
+sub parse_inout_links {
     my ($body) = @_;
     my @links  = ();
     my $link   = qr{\d+
@@ -234,7 +223,7 @@ sub get_inout_links {
             && substr($link_param{link_type}, 0, 6) eq 'modify')
         {
             $link_param{is_param} = 'yes';
-            $link_param{params}   = get_fields($+{link_type});
+            $link_param{params}   = parse_fields($+{link_type});
         }
         push @links, \%link_param;
     }
@@ -242,7 +231,7 @@ sub get_inout_links {
 
 }
 
-sub get_fields {
+sub parse_fields {
     my $body_for_fields = shift;
 
     #p $body_for_fields;
